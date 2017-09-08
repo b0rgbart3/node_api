7cc5f1aba264aa1155ac855b57d29738769637eb
Bart Dority <b0rgBart3@gmail.com>
2017-09-08 11:31:43 -0700

    working get and puts (no db)

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require('http');
 var formidable = require("formidable");
 var util = require("util");
+var url = require('url');
+
 
 var server = http.createServer(function(req, res) {
 
     // Cross Origin Headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+
+    //var queryData = url.parse(req.url, true).query;
 
     // POST DATA
     if (req.method.toLowerCase() == 'post') {
@@ -16,25 +20,49 @@ var server = http.createServer(function(req, res) {
     
     // GET DATA
     if (req.method.toLocaleLowerCase() == 'get') {
-        var data = {
-            data: {
-                languages: [
-                    'English',
-                    'Spanish',
-                    'German',
-                    'Other'
-                ]
-            }
-        };
-        var responseData = JSON.stringify(data);
-        res.end(responseData);
-        console.log("get: ", responseData);
-        return;
+        let data = {};
+        let responseData;
+
+        switch(req.url)
+        {
+            case '/users':
+                data = { data: { users: ['Bart','James']}};
+                responseData = JSON.stringify(data);
+                res.end(responseData);
+                console.log("get: ", responseData);
+                break;
+
+            case '/languages':
+                data = {
+                    data: {
+                        languages: [
+                            'English',
+                            'Spanish',
+                            'German',
+                            'Other'
+                        ]
+                    }
+                };
+                responseData = JSON.stringify(data);
+                res.end(responseData);
+                console.log("get: ", responseData);
+                break;
+
+            default: 
+                console.log("get: got called with no object ref name");
+                res.end();
+                
+                break;
+        }
+
+
     };
 
     res.end();
 
 });
+
+
 
 function processForm(req, res) {
 
