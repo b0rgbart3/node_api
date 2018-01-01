3e25b19c0e846f5e968aeaef0b3b5842b5634781
Bart Dority <b0rgBart3@gmail.com>
2017-12-31 18:47:51 -0800

    adding env port

--- server.js
+++ server.js
@@ -989,11 +989,12 @@ var path = require('path');
 app.use(express.static('public'));
 
 
-var port = 8000;
+//var port = 8000;
 //var port = 3100;
 //app.listen(port);
-server.listen(port,() => console.log('PORT :: ' + port));
+//server.listen(port,() => console.log('PORT :: ' + port));
 
+server.listen(process.env.PORT);
 
 
 // var nodemailer = require('nodemailer');
