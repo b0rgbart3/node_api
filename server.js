0acb4d8504848e9af7e478bfcac8ac64226345dc
Bart Dority <b0rgBart3@gmail.com>
2018-01-06 15:08:11 -0800

    materialfiles

--- server.js
+++ server.js
@@ -951,13 +951,14 @@ app.post('/api/materialfiles', jsonParser, function(req,res,next) {
     uploadMaterialFile(req,res,function(err){
 
         var dest = req.file.destination;
-        console.log('got post request.');
-
+        console.log('got post request: ' + dest);
+        
         if(err){
             console.log('suffered error');
              res.json({error_code:1,err_desc:err});
              return;
         }
+        returnSuccess();
        // returnSuccess();
 //         res.json({error_code:0,err_desc:null});
     });
