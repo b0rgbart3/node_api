62c95796d47128389c2666cb325b4613628aba41
Bart Dority <b0rgBart3@gmail.com>
2018-01-19 15:19:17 -0800

    moving more to the controller ex file

--- server.js
+++ server.js
@@ -246,52 +246,6 @@ var uploadBookImage = multer({ //multer settings
 //     storage: storeDocFile
 // }).single('file');
 
-var getInstructorClasses = function (req,res,next) {
-    console.log("Getting Classes for a specific instructor");
-    dbQuery = {};
-    if (req.query.id && req.query.id != 0)
-    {
-        dbQuery = {'instructors.user_id': req.query.id };
-       
-    }
-
-    res.setHeader('Access-Control-Allow-Origin', ORIGIN_BASEPATH);
-        res.setHeader('Access-Control-Allow-Methods', "POST, GET, PUT, UPDATE, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", 
-        "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
-        db.collection('classes').find(dbQuery).toArray(function(err,docs) {
-            if(err) { handleError(res,err.message, "Failed to get" + resource); }
-            else{
-                res.writeHead(200, {"Content-Type": "application/json"});
-                res.end( JSON.stringify(docs ) );
-                // console.log( JSON.stringify(docs));
-            }
-        });
-};
-
-
-var getStudentClasses = function (req,res,next) {
-    console.log("Getting Classes for a specific student");
-    dbQuery = {};
-    if (req.query.id && req.query.id != 0)
-    {
-        dbQuery = {'students.user_id': req.query.id };
-       
-    }
-
-    res.setHeader('Access-Control-Allow-Origin', ORIGIN_BASEPATH);
-        res.setHeader('Access-Control-Allow-Methods', "POST, GET, PUT, UPDATE, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", 
-        "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
-        db.collection('classes').find(dbQuery).toArray(function(err,docs) {
-            if(err) { handleError(res,err.message, "Failed to get" + resource); }
-            else{
-                res.writeHead(200, {"Content-Type": "application/json"});
-                res.end( JSON.stringify(docs ) );
-                // console.log( JSON.stringify(docs));
-            }
-        });
-};
 
 var getStudents = function (req,res,next) {
     console.log("Getting Students Only");
@@ -438,51 +392,7 @@ var getAllMaterialsByType = function(req,res,next) {
 }
 
 
-var getResources = function(resource,req,res,next) {
 
-    console.log("Getting resource " + resource);
-    dbQuery = {};
-
-    // Use the power of queries to get exactly what we want from the Mongo DB Call
-    if (req.query.id && req.query.id !== 0)
-    {
-        dbQuery = {'id':req.query.id };
-        if (req.query.type && req.query.type !== '')
-        {
-            dbQuery['type'] = req.query.type;
-        } 
-    } else {
-        if (req.query.type && req.query.type !== '')
-        {
-            dbQuery = {'type':req.query.type };
-        } 
-    }
-    
-    if (req.query.classID && (req.query.classID != 0) && req.query.sectionNumber) {
-        dbQuery={'classID': req.query.classID, 'sectionNumber': req.query.sectionNumber };
-        console.log('sectionNumber: ' + req.query.sectionNumber);
-    }
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', "POST, GET, PUT, UPDATE, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", 
-    "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
-    db.collection(resource).find(dbQuery).toArray(function(err,docs) {
-        if(err) { handleError(res,err.message, "Failed to get" + resource); }
-        else{
-            res.writeHead(200, {"Content-Type": "application/json"});
-
-            let stringyDocs = JSON.stringify(docs);
-
-            var reverseChronology = [];
-            if (resource === 'threads') {
-                reverseChronology = sortBy( docs, 'post_date' ).reverse();
-                docs = reverseChronology;
-                res.end( JSON.stringify(docs) );
-            } else {
-            res.end( JSON.stringify(docs ) ); }
-        }
-    });
-};
 
 var deleteResource = function(resource,req,res,next) {
 
