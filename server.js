f6e574dcdc18abee65346fac1d81dccba204a10d
Bart Dority <b0rgBart3@gmail.com>
2018-01-25 19:42:51 -0800

    setting up sendgrid

--- server.js
+++ server.js
@@ -100,6 +100,25 @@ const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 const Sendgrid = require('sendgrid')(SENDGRID_API_KEY);
 
 
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('b0rgBart3@gmail.com');
+var to_email = new helper.Email('bartdority@gmail.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'Hello, Email!');
+var mail = new helper.Mail(from_email, subject, to_email, content);
+
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
+});
+
+sg.API(request, function(error, response) {
+  console.log(response.statusCode);
+  console.log(response.body);
+  console.log(response.headers);
+});
 // function sendEmail(
 //     parentCallback,
 //     fromEmail,
