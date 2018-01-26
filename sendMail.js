706d6235914808c9945561653cc2675ac7e92bf3
Bart Dority <b0rgBart3@gmail.com>
2018-01-26 10:02:55 -0800

    creating external sendMail

--- sendMail.js
+++ sendMail.js
@@ -1,8 +1,21 @@
 
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('thewebsite@reclaimingloom.org');
+var to_email = new helper.Email('bartdority@gmail.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'OK Ive gone back to plain email messages');
+var mail = new helper.Mail(from_email, subject, to_email, content);
+
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
+});
 
 
 
-var sendMail = function(sg, request) {
+var sendMail = function() {
     sg.API(request, function(error, response) {
     console.log(response.statusCode);
     console.log(response.body);
--- server.js
+++ server.js
@@ -65,23 +65,10 @@ const Sendgrid = require('sendgrid')(SENDGRID_API_KEY);
 
 // ---------------------------------
 
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('thewebsite@reclaimingloom.org');
-var to_email = new helper.Email('bartdority@gmail.com');
-var subject = 'Hello World from the SendGrid Node.js Library!';
-var content = new helper.Content('text/plain', 'OK Ive gone back to plain email messages');
-var mail = new helper.Mail(from_email, subject, to_email, content);
-
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON(),
-});
 
 // ---------------------------------
 
-mailer.sendMail(sg, request);
+mailer.sendMail();
 mailer.doSomething();
 
 
