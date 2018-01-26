9455fbce60e7e642820f4b83a3e9a0b0c044fe05
Bart Dority <b0rgBart3@gmail.com>
2018-01-26 10:49:44 -0800

    creating external sendMail

--- sendMail.js
+++ sendMail.js
@@ -1,26 +1,26 @@
 
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('thewebsite@reclaimingloom.org');
-var to_email = new helper.Email('bartdority@gmail.com');
-var subject = 'Hello World from the SendGrid Node.js Library!';
-var textcontent = new helper.Content('text/plain', 'OK Ive gone back to plain email messages');
-var htmlcontent = new helper.Content('text/html', "<h1>Hello HTML World</h1><p>This is an <strong>html</strong> email!")
+// var helper = require('sendgrid').mail;
+// var from_email = new helper.Email('thewebsite@reclaimingloom.org');
+// var to_email = new helper.Email('bartdority@gmail.com');
+// var subject = 'Hello World from the SendGrid Node.js Library!';
+// var textcontent = new helper.Content('text/plain', 'OK Ive gone back to plain email messages');
+// var htmlcontent = new helper.Content('text/html', "<h1>Hello HTML World</h1><p>This is an <strong>html</strong> email!")
 
-// const msg = {
-//     to: 'bartdority@gmail.com',
-//     from: 'info@reclaimingloom.org',
-//     subject: 'sending with SendGrid',
-//     text: 'This is just plain text.',
-//     html: '<h1>Hello HTML World</h1><p>This is an <strong>html</strong> email!',
-// };
+const msg = {
+    to: 'bartdority@gmail.com',
+    from: 'info@reclaimingloom.org',
+    subject: 'sending with SendGrid',
+    text: 'This is just plain text.',
+    html: '<h1>Hello HTML World</h1><p>This is an <strong>html</strong> email!',
+};
 
-var mail = new helper.Mail(from_email, subject, to_email, textcontent, htmlcontent);
+//var mail = new helper.Mail(from_email, subject, to_email, textcontent, htmlcontent);
 
 var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 var request = sg.emptyRequest({
   method: 'POST',
   path: '/v3/mail/send',
-  body: mail.toJSON(),
+  body: msg,
 });
 
 
