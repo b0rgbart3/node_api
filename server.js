db6938b5c1cf82296bccf8cd028cfcaf0ccf3364
Bart Dority <b0rgBart3@gmail.com>
2018-01-25 19:06:52 -0800

    trying to add sendgrid

--- server.js
+++ server.js
@@ -95,6 +95,58 @@ function staticValue (value) {
 
 //var MAU = require('./modify-and-upload');
 
+function sendEmail(
+    parentCallback,
+    fromEmail,
+    toEmails,
+    subject,
+    textContent,
+    htmlContent
+  ) {
+    const errorEmails = [];
+    const successfulEmails = [];
+     const sg = require('sendgrid');
+     sg.setApiKey(process.env.SENDGRID_API_KEY);
+     async.parallel([
+      function(callback) {
+        // Add to emails
+        for (let i = 0; i < toEmails.length; i += 1) {
+          // Add from emails
+          const senderEmail = new helper.Email(fromEmail);
+          // Add to email
+          const toEmail = new helper.Email(toEmails[i]);
+          // HTML Content
+          const content = new helper.Content('text/html', htmlContent);
+          const mail = new helper.Mail(senderEmail, subject, toEmail, content);
+          var request = sg.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: mail.toJSON()
+          });
+          sg.API(request, function (error, response) {
+            console.log('SendGrid');
+            if (error) {
+              console.log('Error response received');
+            }
+            console.log(response.statusCode);
+            console.log(response.body);
+            console.log(response.headers);
+          });
+        }
+        // return
+        callback(null, true);
+      }
+    ], function(err, results) {
+      console.log('Done');
+    });
+    parentCallback(null,
+      {
+        successfulEmails: successfulEmails,
+        errorEmails: errorEmails,
+      }
+    );
+}
+
 
 app.use(logger);
 app.use(sendEmail);
