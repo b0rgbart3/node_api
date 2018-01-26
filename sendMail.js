c4f7df45a55c4b0d32888a97eafba0e394da5518
Bart Dority <b0rgBart3@gmail.com>
2018-01-26 09:48:42 -0800

    setting up sendgrid

--- sendMail.js
+++ sendMail.js
@@ -1,36 +1,22 @@
 
-module.exports = function(request, response, next) {
-
-    console.log('This is flat sendMail.js code running.');
-
-const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
-// const SENDGRID_SENDER = process.env.SENDGRID_SENDER;
-const Sendgrid = require('sendgrid')(SENDGRID_API_KEY);
 
 
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('thewebsite@reclaimingloom.org');
-var to_email = new helper.Email('bartdority@gmail.com');
-var subject = 'Hello World from the SendGrid Node.js Library!';
-var content = new helper.Content('text/plain', 'OK Ive gone back to plain email messages');
-var mail = new helper.Mail(from_email, subject, to_email, content);
 
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON(),
-});
+var sendMail = function(sg) {
+    sg.API(request, function(error, response) {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+    });
+}
 
-sg.API(request, function(error, response) {
-  console.log(response.statusCode);
-  console.log(response.body);
-  console.log(response.headers);
-});
-
-function doSomething() {
+var doSomething = function() {
     console.log('The doSomething funciton got called.');
 }
 
-next(); 
-}
+module.exports.sendMail = sendMail;
+module.exports.doSOmething = doSomething;
+
+
+
+
