938f214b947e4ed2668a5cc1199bb7d9ca9742f4
Bart Dority <b0rgBart3@gmail.com>
2018-04-17 09:25:59 -0700

    allowing PUTS

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ var jsonParser = bodyParser.json();
 var returnSuccess = function( req,res,next) {
     console.log('in return success - origin: ' + origin);
     res.setHeader('Access-Control-Allow-Origin', origin );
-   // res.setHeader('Access-Control-Allow-Methods', "POST, GET, PUT, UPDATE, DELETE, OPTIONS");
+    res.setHeader('Access-Control-Allow-Methods', "POST, GET, PUT, UPDATE, DELETE, OPTIONS");
    // res.setHeader("Access-Control-Allow-Headers", 
   //  "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
   //  res.setHeader("Access-Control-Allow-Credentials", true);
